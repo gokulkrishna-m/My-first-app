3efb4e4b811b1be0749b9772578275f0ba2f74a6
gokulkrishna-m <gokulkrishnamanikumar@gmail.com>
2021-02-10 17:53:30 +0530

    commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
-  selector: '.app-servers',
+  //selector: '.app-servers',
+  selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  s1 = 'No server was created!'
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+
+   }
 
   ngOnInit(): void {
   }
-
+  
+  onServerCreated(){
+    this.s1 = 'Server created successfully...!!';
+  }
 }
