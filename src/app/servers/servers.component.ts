5ec14ce3a51a6cdea9a1408cb760ad0abd2b493f
gokulkrishna-m <gokulkrishnamanikumar@gmail.com>
2021-02-11 18:31:19 +0530

    commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   s1 = 'No server was created!'
-
+  s2 = 'Test Gokul';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,6 +22,11 @@ export class ServersComponent implements OnInit {
   }
   
   onServerCreated(){
-    this.s1 = 'Server created successfully...!!';
+    this.s1 = 'Server created successfully...!! and the Name is '+this.s2;
+  }
+
+  serverUpdate(event:Event){
+    console.log(event);
+    this.s2 = (<HTMLInputElement>event.target).value;
   }
 }
